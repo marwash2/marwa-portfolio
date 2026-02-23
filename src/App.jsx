5e25920e70cba4eda0a36d192b09c302c19891b7
marwash2 <marwashreif4@gmail.com>
2026-02-22 17:43:52 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
